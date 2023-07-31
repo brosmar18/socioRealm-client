56c4123b5026e217fa4e0b73ec2ca291aace64cd
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-07-31 13:58:53 -0500

    ThemeProvider imported

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import LoginPage from "scenes/loginPage";
 import ProfilePage from "scenes/profilePage";
 import { useMemo } from 'react';
 import { useSelector } from "react-redux";
-import { CssBaseline } from "@mui/material";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 import { createTheme } from '@mui/material/styles';
 import { themeSettings } from './theme';
 
