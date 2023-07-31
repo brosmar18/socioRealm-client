5946464b7f80c18389a0a69e4ab09ef507376637
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-07-31 14:06:30 -0500

    imports added

--- src/scenes/navbar/index.jsx
+++ src/scenes/navbar/index.jsx
@@ -1,3 +1,29 @@
+import { useState } from 'react';
+import {
+    Box,
+    IconButton,
+    InputBase,
+    Typography,
+    Select,
+    MenuItem,
+    FormControl,
+    useTheme,
+    useMediaQuery
+} from '@mui/icons-material';
+import {
+    Search, 
+    Message,
+    DarkMode,
+    LightMode,
+    Notifications,
+    Help,
+    Menu,
+    Close
+} from '@mui/icons-material';
+import { useDispatch, useSelector } from 'react-redux';
+import { setMode, setLogout } from 'state';
+import FlexBetween from 'components/FlexBetween';
+
 const NavBar = () => {
     return (
         <div>NavBar Page</div>
