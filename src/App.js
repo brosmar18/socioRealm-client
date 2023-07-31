fc62c768408be0b617b36e0ca9728a2561db2a9a
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-07-31 13:55:27 -0500

    ThemeProvider setup for global use

--- src/App.js
+++ src/App.js
@@ -9,15 +9,22 @@ import { createTheme } from '@mui/material/styles';
 import { themeSettings } from './theme';
 
 function App() {
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
   return <div className='app'>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/home" element={<HomgePage />} />
-        <Route path="/profile/:userId" element={<ProfilePage />} />
-      </Routes>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/home" element={<HomgePage />} />
+          <Route path="/profile/:userId" element={<ProfilePage />} />
+        </Routes>
+      </ThemeProvider>
     </BrowserRouter>
-  </div>
+
+  </div >
 }
 
 export default App;
