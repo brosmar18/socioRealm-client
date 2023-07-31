06be805f8bd1357a2de51f419b4d5e244414cf9a
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-07-31 13:53:17 -0500

    imports added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
 import HomgePage from 'scenes/homePage';
 import LoginPage from "scenes/loginPage";
 import ProfilePage from "scenes/profilePage";
+import { useMemo } from 'react';
+import { useSelector } from "react-redux";
+import { CssBaseline } from "@mui/material";
+import { createTheme } from '@mui/material/styles';
+import { themeSettings } from './theme';
 
 function App() {
   return <div className='app'>
