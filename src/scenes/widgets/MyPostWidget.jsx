51b9cbc3e853bb0ac65f3ce5a4e4c76af38c3e4c
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-08-01 17:46:05 -0500

    add post section added

--- src/scenes/widgets/MyPostWidget.jsx
+++ src/scenes/widgets/MyPostWidget.jsx
@@ -6,8 +6,8 @@ import {
     ImageOutlined,
     MicOutlined,
     MoreHorizOutlined,
-  } from "@mui/icons-material";
-  import {
+} from "@mui/icons-material";
+import {
     Box,
     Divider,
     Typography,
@@ -16,16 +16,16 @@ import {
     Button,
     IconButton,
     useMediaQuery,
-  } from "@mui/material";
-  import FlexBetween from "components/FlexBetween";
-  import Dropzone from "react-dropzone";
-  import UserImage from "components/UserImage";
-  import WidgetWrapper from "components/WidgetWrapper";
-  import { useState } from "react";
-  import { useDispatch, useSelector } from "react-redux";
-  import { setPosts } from "state";
+} from "@mui/material";
+import FlexBetween from "components/FlexBetween";
+import Dropzone from "react-dropzone";
+import UserImage from "components/UserImage";
+import WidgetWrapper from "components/WidgetWrapper";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setPosts } from "state";
 
-  const MyPostWidget = ({picturePath}) => {
+const MyPostWidget = ({ picturePath }) => {
     const dispatch = useDispatch();
     const [isImage, setIsImage] = useState(false);
     const [image, setImage] = useState(null);
@@ -37,7 +37,24 @@ import {
     const mediumMain = palette.neutral.mediumMain;
     const medium = palette.neutral.medium;
 
-    
-  };
+    return (
+        <WidgetWrapper>
+            <FlexBetween gap="1.5rem">
+                <UserImage image={picturePath} />
+                <InputBase
+                    placeholder="What's on your mind..."
+                    onChange={(e) => setPost(e.target.value)}
+                    value={post}
+                    sx={{
+                        width: "100%",
+                        backgroundColor: palette.neutral.light,
+                        borderRadius: "2rem",
+                        padding: "1rem 2rem",
+                    }}
+                />
+            </FlexBetween>
+        </WidgetWrapper>
+    );
+};
 
-  export default MyPostWidget;
+export default MyPostWidget;
