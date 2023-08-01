0191a7f55e3511e15f88399cf7906c2dccbdf709
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-08-01 18:42:07 -0500

    patchLike built to fetch postId

--- src/scenes/widgets/PostWidget.jsx
+++ src/scenes/widgets/PostWidget.jsx
@@ -3,16 +3,16 @@ import {
     FavoriteBorderOutlined,
     FavoriteOutlined,
     ShareOutlined,
-  } from "@mui/icons-material";
-  import { Box, Divider, IconButton, Typography, useTheme } from "@mui/material";
-  import FlexBetween from "components/FlexBetween";
+} from "@mui/icons-material";
+import { Box, Divider, IconButton, Typography, useTheme } from "@mui/material";
+import FlexBetween from "components/FlexBetween";
 //   import Friend from "components/Friend";
-  import WidgetWrapper from "components/WidgetWrapper";
-  import { useState } from "react";
-  import { useDispatch, useSelector } from "react-redux";
-  import { setPost } from "state";
+import WidgetWrapper from "components/WidgetWrapper";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setPost } from "state";
 
-  const PostWidget = ({
+const PostWidget = ({
     postId,
     postUserId,
     name,
@@ -22,23 +22,37 @@ import {
     userPicturePath,
     likes,
     comments,
-  }) => {
+}) => {
     const [isComments, setIsComments] = useState(false);
     const dispatch = useDispatch();
     const token = useSelector((state) => state.token);
     const loggedInUserId = useSelector((state) => state.user._id);
     const isLiked = Boolean(likes[loggedInUserId]);
     const likeCount = Object.keys(likes).length;
-  
+
     const { palette } = useTheme();
     const main = palette.neutral.main;
     const primary = palette.primary.main;
 
-  return (
-    <div>
-      
-    </div>
-  )
+    const patchLike = async () => {
+        const response = await fetch(`http://localhost:3001/posts/${postId}/like`, {
+            method: "PATCH",
+            headers: {
+                Authorization: `Bearer ${token}`,
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ userId: loggedInUserId }),
+        });
+        const updatedPost = await response.json();
+        dispatch(setPost({ post: updatedPost }));
+    };
+
+
+    return (
+        <div>
+
+        </div>
+    )
 }
 
 export default PostWidget
