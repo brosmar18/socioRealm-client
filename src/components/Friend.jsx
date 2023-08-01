99426ae4be84586f08171d5deeeca9fd7a418ecd
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-08-01 18:50:28 -0500

    patchFriend added to fetch _id and friendId

--- src/components/Friend.jsx
+++ src/components/Friend.jsx
@@ -13,21 +13,36 @@ const Friend = ({ friendId, name, subtitle, userPicturePath }) => {
     const { _id } = useSelector((state) => state.user);
     const token = useSelector((state) => state.token);
     const friends = useSelector((state) => state.user.friends);
-  
+
     const { palette } = useTheme();
     const primaryLight = palette.primary.light;
     const primaryDark = palette.primary.dark;
     const main = palette.neutral.main;
     const medium = palette.neutral.medium;
-  
+
     const isFriend = friends.find((friend) => friend._id === friendId);
 
-    
-  return (
-    <div>
-      
-    </div>
-  )
+    const patchFriend = async () => {
+        const response = await fetch(
+            `http://localhost:3001/users/${_id}/${friendId}`,
+            {
+                method: "PATCH",
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                    "Content-Type": "application/json",
+                },
+            }
+        );
+        const data = await response.json();
+        dispatch(setFriends({ friends: data }));
+    };
+
+
+    return (
+        <div>
+
+        </div>
+    )
 }
 
 export default Friend
