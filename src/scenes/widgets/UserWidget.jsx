cfba59f70367e6695b4f61f080ec2d07045a734d
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-08-01 16:47:31 -0500

    Social Profiles section added

--- src/scenes/widgets/UserWidget.jsx
+++ src/scenes/widgets/UserWidget.jsx
@@ -115,7 +115,15 @@ const UserWidget = ({ userId, picturePath }) => {
                     </Typography>
                 </FlexBetween>
             </Box>
+            
+            <Divider />
 
+              {/* FOURTH ROW */}
+              <Box p="1rem 0">
+              <Typography fontSize="1rem" color={main} fontWeight="500" mb="1rem">
+          Social Profiles
+        </Typography>
+              </Box>
 
         </WidgetWrapper>
     )
