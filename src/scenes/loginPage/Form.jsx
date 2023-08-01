399b61439a27c942604f74d02079470c4a42b278
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-07-31 20:00:59 -0500

    register function built

--- src/scenes/loginPage/Form.jsx
+++ src/scenes/loginPage/Form.jsx
@@ -55,6 +55,33 @@ const Form = () => {
     const isLogin = pageType === "login";
     const isRegister = pageType === "register";
 
+    const register = async (values, onSubmitProps) => {
+        // this allows us to send form info with image
+        const formData = new FormData();
+        for ( let value in values) {
+            formData.append(value, values[value])
+        }
+        formData.append('picturePath', values.picture.name);
+
+        const savedUserResponse = await fetch(
+            "http://localhost:3001/auth/register",
+            {
+                method: "POST",
+                body: formData,
+            }
+        );
+        const savedUser = await savedUserResponse.json();
+        onSubmitProps.resetForm();
+        
+        if (savedUser) {
+            setPageType("login");
+        }
+    };
+
+    const login = async (values, onSubmitProps) => {
+        
+    }
+
     const handleFormSubmit = async (values, onSubmitProps) => {
         if (isLogin) await login(values, onSubmitProps):
         if (isRegister) await register(values, onSubmitProps):
