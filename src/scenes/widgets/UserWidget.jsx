c8aca7c7e9dc5604a1c5d710cd3b1f8b64b1f64d
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-08-01 16:48:47 -0500

    LinkdIn section added

--- src/scenes/widgets/UserWidget.jsx
+++ src/scenes/widgets/UserWidget.jsx
@@ -135,7 +135,18 @@ const UserWidget = ({ userId, picturePath }) => {
                     </FlexBetween>
                     <EditOutlined sx={{ color: main }} />
                 </FlexBetween>
-
+                <FlexBetween gap="1rem">
+                    <FlexBetween gap="1rem">
+                        <img src="../assets/linkedin.png" alt="linkedin" />
+                        <Box>
+                            <Typography color={main} fontWeight="500">
+                                Linkedin
+                            </Typography>
+                            <Typography color={medium}>Network Platform</Typography>
+                        </Box>
+                    </FlexBetween>
+                    <EditOutlined sx={{ color: main }} />
+                </FlexBetween>
             </Box>
 
         </WidgetWrapper>
