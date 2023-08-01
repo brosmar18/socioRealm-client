d8d55b49d109dc4c09a494e5d103c6984e32bba8
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-08-01 18:49:59 -0500

    hooks and variables added

--- src/components/Friend.jsx
+++ src/components/Friend.jsx
@@ -8,6 +8,21 @@ import UserImage from "./UserImage";
 
 
 const Friend = ({ friendId, name, subtitle, userPicturePath }) => {
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const { _id } = useSelector((state) => state.user);
+    const token = useSelector((state) => state.token);
+    const friends = useSelector((state) => state.user.friends);
+  
+    const { palette } = useTheme();
+    const primaryLight = palette.primary.light;
+    const primaryDark = palette.primary.dark;
+    const main = palette.neutral.main;
+    const medium = palette.neutral.medium;
+  
+    const isFriend = friends.find((friend) => friend._id === friendId);
+
+    
   return (
     <div>
       
