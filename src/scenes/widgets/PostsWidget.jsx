cfbb5a239c93e60734e28c100d44247f0827600f
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-08-01 18:26:32 -0500

    getPosts async function built to fetch posts data

--- src/scenes/widgets/PostsWidget.jsx
+++ src/scenes/widgets/PostsWidget.jsx
@@ -9,7 +9,14 @@ const PostsWidget = () => {
     const posts = useSelector((state) => state.posts);
     const token = useSelector((state) => state.token);
     
-
+    const getPosts = async () => {
+        const response = await fetch("http://localhost:3001/posts", {
+            method: "GET",
+            headers: { Authorization: `Bearer ${token}`},
+        });
+        const data = await response.json();
+        dispatch(setPosts({ posts: data }));
+    };
   return (
     <>
     
