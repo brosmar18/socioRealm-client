98de5bec9d0b660d5500b4d09fc11f28b881c67a
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-08-01 17:44:41 -0500

    hooks and variables added

--- src/scenes/widgets/MyPostWidget.jsx
+++ src/scenes/widgets/MyPostWidget.jsx
@@ -24,3 +24,20 @@ import {
   import { useState } from "react";
   import { useDispatch, useSelector } from "react-redux";
   import { setPosts } from "state";
+
+  const MyPostWidget = ({picturePath}) => {
+    const dispatch = useDispatch();
+    const [isImage, setIsImage] = useState(false);
+    const [image, setImage] = useState(null);
+    const [post, setPost] = useState("");
+    const { palette } = useTheme();
+    const { _id } = useSelector((state) => state.user);
+    const token = useSelector((state) => state.token);
+    const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
+    const mediumMain = palette.neutral.mediumMain;
+    const medium = palette.neutral.medium;
+
+    
+  };
+
+  export default MyPostWidget;
