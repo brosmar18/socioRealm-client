b1025a0ef7d30b922c5c3b1928a27a2849d225f5
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-08-01 16:24:33 -0500

    hooks and variables setup

--- src/scenes/widgets/UserWidget.jsx
+++ src/scenes/widgets/UserWidget.jsx
@@ -11,3 +11,13 @@ import WidgetWrapper from "components/WidgetWrapper";
 import { useSelector } from "react-redux";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+
+const UserWidget = ({ userId, picturePath }) => {
+    const [user, setUser] = useState(null);
+    const { palette } = useTheme();
+    const navigate = useNavigate();
+    const token = useSelector((state) => state.token);
+    const dark = palette.neutral.dark;
+    const medium = palette.neutral.medium;
+    const main = palette.neutral.main;
+}
