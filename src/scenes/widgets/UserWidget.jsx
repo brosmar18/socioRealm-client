014339dd2f0b9f9c3745903044d85e2f18c28d94
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-08-01 16:46:47 -0500

    impressions of post section added

--- src/scenes/widgets/UserWidget.jsx
+++ src/scenes/widgets/UserWidget.jsx
@@ -108,6 +108,12 @@ const UserWidget = ({ userId, picturePath }) => {
                         {viewedProfile}
                     </Typography>
                 </FlexBetween>
+                <FlexBetween>
+                    <Typography color={medium}>Impressions of your post</Typography>
+                    <Typography color={main} fontWeight="500">
+                        {impressions}
+                    </Typography>
+                </FlexBetween>
             </Box>
 
 
