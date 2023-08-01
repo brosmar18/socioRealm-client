538436dc4aed1069c2a043bb05f16a4bb4d5161e
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-08-01 18:23:48 -0500

    basic func component built and imports added

---
+++ src/scenes/widgets/PostsWidget.jsx
@@ -0,0 +1,15 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setPosts } from "state";
+import PostWidget from "./PostWidget";
+
+
+const PostsWidget = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default PostsWidget
