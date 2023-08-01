1761dfbc1578169be22523c55b494e08d60b5672
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-08-01 16:34:42 -0500

    first and llast name section added

--- src/scenes/widgets/UserWidget.jsx
+++ src/scenes/widgets/UserWidget.jsx
@@ -59,6 +59,21 @@ const UserWidget = ({ userId, picturePath }) => {
             >
                 <FlexBetween gap="1rem">
                     <UserImage image={picturePath} />
+                    <Box>
+                        <Typography
+                            variant="h4"
+                            color={dark}
+                            fontWeight="500"
+                            sx={{
+                                "&:hover": {
+                                    color: palette.primary.light,
+                                    cursor: "pointer",
+                                },
+                            }}
+                        >
+                            {firstName} {lastName}
+                        </Typography>
+                    </Box>
                 </FlexBetween>
             </FlexBetween>
         </WidgetWrapper>
