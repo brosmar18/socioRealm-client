b0e2311bb6886765d9dcc400ba767ac0676819b4
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-08-01 19:23:09 -0500

    styles added

--- src/scenes/profilePage/index.jsx
+++ src/scenes/profilePage/index.jsx
@@ -17,7 +17,7 @@ const ProfilePage = () => {
     const getUser = async () => {
         const response = await fetch(`http://localhost:3001/users/${userId}`, {
             method: "GET",
-            headers: { Authorization: `Bearer ${token}`},
+            headers: { Authorization: `Bearer ${token}` },
         });
         const data = await response.json();
         setUser(data);
@@ -25,14 +25,23 @@ const ProfilePage = () => {
 
     useEffect(() => {
         getUser();
-      }, []); 
+    }, []);
 
-      if (!user) return null;
+    if (!user) return null;
 
     return (
         <Box>
             <Navbar />
-        </Box>      
+            <Box
+                width="100%"
+                padding="2rem 6%"
+                display={isNonMobileScreens ? "flex" : "block"}
+                gap="2rem"
+                justifyContent="center"
+            >
+
+            </Box>
+        </Box>
     );
 };
 
